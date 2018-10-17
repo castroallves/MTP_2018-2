b703d071b1c6d011864f19e125c1a794784560d0
pedd240 <30752706+pedd240@users.noreply.github.com>
2018-10-16 23:38:51 -0300

    Add files via upload

---
+++ P3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+int i, sum = 0;
+
+char num[256];
+
+scanf("%s", num);
+
+for(i = 0; num[i] != '\0'; i++){
+if(num[i] >= '0' && num[i] <= '9')
+sum = sum *10 + (num[i] - '0');
+}
+
+printf("%d\n", sum);
+
+}
